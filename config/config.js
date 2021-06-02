0651cfa2415475ab3029fa8f69d99c2b2f1c0878
0BlackSmoke0 <bertucci.henrique@gmail.com>
2021-06-02 08:51:45 -0300

    adcionando o modelo

--- config/config.js
+++ config/config.js
@@ -26,7 +26,7 @@ module.exports = {
   dev: {
     // altere APENAS username, password e database.
     username: 'root',
-    password: '1601',
+    password: 'bandtec',
     database: 'csinfo',
     host: '127.0.0.1',
     dialect: 'mysql',
